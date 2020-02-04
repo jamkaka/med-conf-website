ca91bae534e15fe26fd76e8cb2dafdebf6cd1b02
unknown <jamkaka.mk@gmail.com>
2020-02-04 22:32:08 +0100

    removed jump.js and substituted with smoothscroll

--- js/main.js
+++ js/main.js
@@ -1,57 +1,56 @@
-// jump.js 
-// jump.js build using npm package 'lio' and embedded via <script> tags. module available via jumpJS function.
-import jump from 'jump.js'
-
-const links = document.querySelectorAll('.js-link');
-links.forEach(link => link.addEventListener('click', smoothScroll));
+const links = document.querySelectorAll(".js-link");
+links.forEach(link => link.addEventListener("click", smoothScroll));
 
 function smoothScroll(e) {
+  const scroll = new SmoothScroll();
   const clickedDataTarget = e.currentTarget.dataset.target;
-  jump(`.section-${clickedDataTarget}`);
-};
+  const anchor = document.querySelector(`.section-${clickedDataTarget}`);
+  scroll.animateScroll(anchor);
+}
 
 // lectures section component.
-let activeLecture = $('#lecture-1');
-const lectures = $('.lecture-single')
+let activeLecture = $("#lecture-1");
+const lectures = $(".lecture-single");
 
-$('.lecture-list').on('click', changeFocusedLecture)
+$(".lecture-list").on("click", changeFocusedLecture);
 
 function changeFocusedLecture(e) {
-  if ($(e.target).hasClass('lecture-single')) {
+  if ($(e.target).hasClass("lecture-single")) {
     const clickedLecture = $(`#lecture-${e.target.id}`);
     // prevent key mashing.
-    if (activeLecture.is(':animated')) return;
-    if(!clickedLecture.is(activeLecture)) {
+    if (activeLecture.is(":animated")) return;
+    if (!clickedLecture.is(activeLecture)) {
       // add class to show which lecture is selected.
-      lectures.each((id, lecture) => $(lecture).removeClass('is-lecture-selected'));
-      activeLecture.removeClass('is-lecture-selected');
-      $(e.target).addClass('is-lecture-selected');
+      lectures.each((id, lecture) =>
+        $(lecture).removeClass("is-lecture-selected")
+      );
+      activeLecture.removeClass("is-lecture-selected");
+      $(e.target).addClass("is-lecture-selected");
       // animation part.
       activeLecture.fadeOut("slow", function() {
-        clickedLecture.fadeIn("slow").css({'display':'flex'});
-        activeLecture=clickedLecture;
+        clickedLecture.fadeIn("slow").css({ display: "flex" });
+        activeLecture = clickedLecture;
       });
     }
   }
 }
 
 // particles.js
-particlesJS.load('particles-js', 'js/particles.json');
+particlesJS.load("particles-js", "js/particles.json");
 
 // hamburger menu.
-$('.menu-hamburger').on('click', openResponsiveMenu);
+$(".menu-hamburger").on("click", openResponsiveMenu);
 
 function openResponsiveMenu() {
-  $('.overlay').fadeIn(300);
-  $('.menu-hamburger').addClass('is-menu-hamburger-clicked');
-  $('.responsive-menu').addClass('is-responsive-menu-toggled');
+  $(".overlay").fadeIn(300);
+  $(".menu-hamburger").addClass("is-menu-hamburger-clicked");
+  $(".responsive-menu").addClass("is-responsive-menu-toggled");
 }
 
-$('.overlay, .js-link').on('click', closeResponsiveMenu)
+$(".overlay, .js-link").on("click", closeResponsiveMenu);
 
 function closeResponsiveMenu() {
-  $('.overlay').fadeOut(300);
-  $('.menu-hamburger').removeClass('is-menu-hamburger-clicked');
-  $('.responsive-menu').removeClass('is-responsive-menu-toggled');
+  $(".overlay").fadeOut(300);
+  $(".menu-hamburger").removeClass("is-menu-hamburger-clicked");
+  $(".responsive-menu").removeClass("is-responsive-menu-toggled");
 }
-
